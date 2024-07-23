6baa4870d1e4b73d18bc287c977a2edda1ae9349
M4cr1b <159058241+M4cr1b@users.noreply.github.com>
2024-07-23 00:21:49 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,46 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    char op;
-    float num1, num2;
+    int num1, num2;
+    double add, sub, mult, div, exp, mod;
+    string operation;
 
-    // Prompt the user to enter an operator
-    std::cout << "Enter operator (+, -, *, /): ";
-    std::cin >> op;
+    cout<< "Enter the first number: ";
+    cin>> num1;
+    cout<< "Enter the second number: ";
+    cin>> num2;
 
-    // Prompt the user to enter two operands
-    std::cout << "Enter two numbers: ";
-    std::cin >> num1 >> num2;
+    cout<< "Arithmetic Operations:"<<endl;
+    cout<< "1. Addition"<<endl;
+    cout<< "2. Subtraction"<<endl;
+    cout<< "3. Multiplication"<<endl;
+    cout<< "4. Division"<<endl;
+    cout<< "5. Exponent"<<endl;
+    cout<< "6. Modulus"<<endl;
 
-    // Perform the operation based on the operator entered
-    switch(op) {
-        case '+':
-            std::cout << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
-            break;
-        case '-':
-            std::cout << num1 << " - " << num2 << " = " << num1 - num2 << std::endl;
-            break;
-        case '*':
-            std::cout << num1 << " * " << num2 << " = " << num1 * num2 << std::endl;
-            break;
-        case '/':
-            if (num2 != 0)
-                std::cout << num1 << " / " << num2 << " = " << num1 / num2 << std::endl;
-            else
-                std::cout << "Error! Division by zero." << std::endl;
-            break;
-        default:
-            std::cout << "Error! Operator is not correct." << std::endl;
-            break;
-    }
+    cout<< "Enter the arithmetic operation: ";
+    cin>> operation;
 
+    if(operation == "Addition" || operation == "1"){
+        add = num1 + num2;
+        cout<< "The sum of the two numbers is "<<add<<endl;
+    }else if (operation == "Subtraction" || operation == "2"){
+        sub = num1 - num2;
+        cout<< "The difference between the two numbers is "<<sub<<endl;
+    }else if (operation == "Multiplication" || operation == "3"){
+        mult = num1 * num2;
+        cout<< "The product of the two numbers is "<<mult<<endl;
+    }else if (operation == "Division" || operation == "4"){
+        div = num1 / num2;
+        cout<< "The division of the two numbers is "<<div<<endl;
+    }else if (operation == "Exponent" || operation == "5"){
+        exp = num1 ^ num2;
+        cout<< "The exponent of the first number against the second number is "<<div<<endl;
+    }else if (operation == "Modulus" || operation == "6"){
+        exp = num1 % num2;
+        cout<< "The modulus of the first number against the second number is "<<mod<<endl;
+    }else { cout<< "Invalid operation chosen!!";}
     return 0;
 }
